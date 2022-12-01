0872f1b278d8d7bd566249960cbcf08a8605b9e6
Muhammad Nouman Munir <noumanmunir458@gmail.com>
2022-12-02 01:47:49 +0500

    User Guard Updated and field id added in User schema

--- src/guard/user.guard.ts
+++ src/guard/user.guard.ts
@@ -1,6 +1,4 @@
-import { userDocument } from './../user/user.schema';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { map, Observable } from 'rxjs';
 import { User } from 'src/user/user.schema';
 import { UserService } from 'src/user/user.service';
 
@@ -8,28 +6,20 @@ import { UserService } from 'src/user/user.service';
 export class UserGuard implements CanActivate {
   constructor(private readonly service: UserService) {}
 
-  // hasPermission: boolean = false;
-  // dbUser:User
-
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
+
     const params = request.params;
+
     const user: User = request.user.user;
 
-    console.log('USER FROM REQUEST', user);
-
-    console.log('Params ID', params.id);
-
     const dbUser: User = await this.service.findOneById(user._id);
-    console.log('DB USER ID', dbUser._id);
-    let hasPermission = false;
+    let hasPermission: boolean = false;
     if (dbUser._id == params.id) {
-      console.log('IT WORKS TILL HERE');
-
       hasPermission = true;
-
-      console.log('IT WORKS TILL HERE AS WELL');
+      return user && hasPermission;
+    } else {
+      throw new Error("403 Forbidden! You Don't Have Access");
     }
-    return user && hasPermission;
   }
 }
